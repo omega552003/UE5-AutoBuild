e4b929ee2cc84acd42f62e407d80157b1129ff22
omega552003 <omega552003@users.noreply.github.com>
2022-07-27 15:54:51 -0400

    Update UE5-AutoBuild.bat
    Fixed some conventions

--- UE5-AutoBuild.bat
+++ UE5-AutoBuild.bat
@@ -1,33 +1,33 @@
 @ECHO OFF
 
 REM This is supposed to identify if its updating or installing the engine.
-if exist UnrealEngine\ (
+IF EXIST UnrealEngine\ (
   ECHO "Unreal Engine found, updating."
-  cd UnrealEngine
+  CD UnrealEngine
   REM Setting current location to var
-  set "ueinstallloc=%cd%"
-  echo %ueinstallloc%
+  SET "UEInstallDir=%cd%"
+  ECHO %UEInstallDir%
   git remote add origin https://github.com/EpicGames/UnrealEngine.git
   git fetch
   git reset --hard HEAD
   git merge origin/release
   git pull origin release
-) else (
+) ELSE (
   ECHO "Unreal Engine not found, cloning."
   git clone -b release https://github.com/EpicGames/UnrealEngine.git
-  cd UnrealEngine
+  CD UnrealEngine
   REM Setting current location to var
-  set "ueinstallloc=%cd%"
-  echo %ueinstallloc%
+  SET "UEInstallDir=%cd%"
+  ECHO %UEInstallDir%
 )
 
 REM Run UE5's build scripts
-call Setup.bat
-call GenerateProjectFiles.bat
+CALL Setup.bat
+CALL GenerateProjectFiles.bat
 
 REM Finding location of MSbuild.exe
-FOR /F "delims=" %i IN ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe') DO set vsinstallloc=%i
-echo %vsinstallloc%
+FOR /F "delims=" %i IN ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe') DO set VSInstallDir=%i
+ECHO %VSInstallDir%
 
 REM Build project in VS commandline
-"%vsinstallloc%" -m /t:Engine\UE5 /p:Configuration="Development Editor" /p:Platform=Win64 UE5.sln
+"%VSInstallDir%" -m /t:Engine\UE5 /p:Configuration="Development Editor" /p:Platform=Win64 UE5.sln
