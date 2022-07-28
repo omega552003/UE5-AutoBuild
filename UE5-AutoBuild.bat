d06c910f392b5135946326eb491452947b74f3d7
omega552003 <omega552003@users.noreply.github.com>
2022-07-27 22:42:40 -0400

    Update UE5-AutoBuild.bat
    Rewritten to be "smarter"

--- UE5-AutoBuild.bat
+++ UE5-AutoBuild.bat
@@ -1,33 +1,75 @@
 @ECHO OFF
+SET "UEInstallDir="
+SET "UErepo="
+SET "UEStat="
+SET "VSInstallDir="
+
+SET UErepo="https://github.com/EpicGames/UnrealEngine.git"
+
+REM Prereqs
+REM Finding location of MSbuild.exe
+FOR /F "delims=" %%i IN ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe') DO (
+SET VSInstallDir="%%i"
+ECHO MSBUILD found at: 
+ECHO %VSInstallDir%
+)
 
 REM This is supposed to identify if its updating or installing the engine.
 IF EXIST UnrealEngine\ (
-  ECHO "Unreal Engine found, updating."
-  CD UnrealEngine
-  REM Setting current location to var
-  SET "UEInstallDir=%cd%"
-  ECHO %UEInstallDir%
-  git remote add origin https://github.com/EpicGames/UnrealEngine.git
-  git fetch
-  git reset --hard HEAD
-  git merge origin/release
-  git pull origin release
+ECHO Unreal Engine found, updating.
+SET UEStat=u
+ECHO %UEStat%
+GOTO update
 ) ELSE (
-  ECHO "Unreal Engine not found, cloning."
-  git clone -b release https://github.com/EpicGames/UnrealEngine.git
-  CD UnrealEngine
-  REM Setting current location to var
-  SET "UEInstallDir=%cd%"
-  ECHO %UEInstallDir%
+ECHO Unreal Engine not found, cloning.
+SET UEStat=c
+ECHO %UEStat%
+GOTO clone
 )
 
+:UEDir
+REM Setting current working directory to Unreal Engine source root 
+CD UnrealEngine
+SET UEInstallDir="%cd%"
+ECHO %UEInstallDir%
+IF %UEStat%==c (
+SET "UEStat="
+GOTO build
+)
+IF %UEStat%==u (
+SET "UEStat="
+GOTO update
+)
+
+:clone
+IF %UEStat%==c (
+GIT clone -b release %UErepo%
+GOTO UEDir
+)
+
+:update
+IF %UEStat%==u (
+GOTO UEDir
+) ELSE (
+GIT remote add origin %UErepo%
+GIT fetch
+GIT reset --hard HEAD
+GIT merge origin/release
+GIT pull origin release
+)
+pause
+
+:build
 REM Run UE5's build scripts
 CALL Setup.bat
 CALL GenerateProjectFiles.bat
-
-REM Finding location of MSbuild.exe
-FOR /F "delims=" %i IN ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe') DO SET VSInstallDir=%i
-ECHO %VSInstallDir%
-
 REM Build project in VS commandline
-"%VSInstallDir%" -m /t:Engine\UE5 /p:Configuration="Development Editor" /p:Platform=Win64 UE5.sln
+%VSInstallDir% -m /t:Engine\UE5 /p:Configuration="Development Editor" /p:Platform=Win64 UE5.sln
+
+:end
+SET "UEInstallDir="
+SET "UErepo="
+SET "UEStat="
+SET "VSInstallDir="
+pause
+GOTO:eof
